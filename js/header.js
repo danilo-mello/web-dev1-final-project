32926ddd3cde0aeed703c7b211c9482c151657f4
heymiriam <miriambellon@gmail.com>
2020-12-16 23:07:38 -0800

    add animation to nav

--- js/header.js
+++ js/header.js
@@ -12,3 +12,14 @@ $(function () {
     }, 200);
   });
 });
+
+$(function() {
+  $(window).on("scroll", function() {
+      if($(window).scrollTop() > 50) {
+          $(".header").addClass("active");
+      } else {
+          //remove the background property so it comes transparent again (defined in your css)
+         $(".header").removeClass("active");
+      }
+  });
+});
